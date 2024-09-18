ca65ac289db16f53fda5fa05e6b599d70498e4fa
riufix <enguerrand.cousin@gmail.com>
2024-09-18 15:57:04 +0200

    [ADD]: character class + Game mod class

---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Private/Characters/SmashCharacter.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/SmashCharacter.h"
+
+//#include "Character/SmashCharacterStateMachine.h"
+
+// Sets default values
+
+// Called when the game starts or when spawned
+
+
+ASmashCharacter::ASmashCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASmashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	//CreateStateMachine();
+
+	//InitStateMachine();
+	
+}
+
+// Called every frame
+void ASmashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASmashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	//RotateMeshUsingOrientX();
+}
+/*
+float ASmashCharacter::GetOrientX() const
+{
+	return OrientX;
+}
+
+void ASmashCharacter::SetOrientX(float NewOrientX)
+{
+	OrientX = NewOrientX;
+}
+
+void ASmashCharacter::RotateMeshUsingOrientX() const
+{
+	FRotator Rotation = GetMesh()->GetRelativeRotation();
+	Rotation.Yaw = -90.f * OrientX;
+	GetMesh()->SetRelativeRotation(Rotation);
+}
+
+/*void ASmashCharacter::CreateStateMachine()
+{
+	StateMachine = NewObject<USmashCharacterStateMachine>(this);
+}
+
+void ASmashCharacter::InitStateMachine()
+{
+	if(StateMachine == nullptr) return;
+	StateMachine->Init(this);
+}*/
---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/Character/SmashCharacter.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SmashCharacter.generated.h"
+
+class USmashCharacterStateMachine;
+
+UCLASS()
+class SMASHUE_API ASmashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+#pragma  region Unreal Default
+	
+public:
+	// Sets default values for this character's properties
+	ASmashCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/*
+#pragma  endregion Unreal Default
+
+#pragma  region Orient
+public:
+	float GetOrientX() const;
+	
+	void SetOrientX(float NewOrientX);
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	float OrientX = 1.f;
+
+	void  RotateMeshUsingOrientX() const;
+	
+#pragma endregion Orient
+
+#pragma  region State Machine
+
+public:
+	void CreateStateMachine();
+
+	void InitStateMachine();
+
+protected:
+	UPROPERTY()
+	TObjectPtr<USmashCharacterStateMachine> StateMachine;
+	
+#pragma endregion State Machine
+	*/
+};
